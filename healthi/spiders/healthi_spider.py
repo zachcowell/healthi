28173408fc77b776fe644e05b3a28e5f0b231551
zachcowell <zach.cowell@gmail.com>
2014-01-04 01:44:29 -0500

    playing around with selectors in shell, placed zip code selector in spider file

--- healthi/spiders/healthi_spider.py
+++ healthi/spiders/healthi_spider.py
@@ -1,7 +1,7 @@
-from scrapy.spider import Spider
+from scrapy.spider import BaseSpider
 
-class HealthiSpider(Spider):
-    name = "dmoz"
+class HealthiSpider(BaseSpider):
+    name = "healthi"
     allowed_domains = ["dmoz.org"]
     start_urls = [
         "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
@@ -11,3 +11,5 @@ class HealthiSpider(Spider):
     def parse(self, response):
         filename = response.url.split("/")[-2]
         open(filename, 'wb').write(response.body)
+
+#sel.xpath('//div[@id="uiSearchZip"]').xpath('ul/li/a/@href').extract()
