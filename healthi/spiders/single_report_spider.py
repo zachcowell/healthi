76ad6c63cff0ff85e0f973f1046ae013c87f4acf
Zach Cowell <zach.cowell@gmail.com>
2014-01-07 23:51:26 -0500

    fixed index out of bounds issue with some of the nodes

--- healthi/spiders/single_report_spider.py
+++ healthi/spiders/single_report_spider.py
@@ -6,44 +6,50 @@ class HealthiSpider(BaseSpider):
 	name = "single"
 	allowed_domains = ["gegov.com"]
 	start_urls = [
-		"http://washington.dc.gegov.com/webadmin/dhd_431/lib/mod/inspection/paper/_paper_food_inspection_report.cfm?inspectionID=200360&wguid=1367&wgunm=sysact&wgdmn=431",
-		"http://washington.dc.gegov.com/webadmin/dhd_431/lib/mod/inspection/paper/_paper_food_inspection_report.cfm?inspectionID=198305&wguid=1367&wgunm=sysact&wgdmn=431"
+		#"http://washington.dc.gegov.com/webadmin/dhd_431/lib/mod/inspection/paper/_paper_food_inspection_report.cfm?inspectionID=200360&wguid=1367&wgunm=sysact&wgdmn=431",
+		#"http://washington.dc.gegov.com/webadmin/dhd_431/lib/mod/inspection/paper/_paper_food_inspection_report.cfm?inspectionID=198305&wguid=1367&wgunm=sysact&wgdmn=431",
+		"http://washington.dc.gegov.com/webadmin/dhd_431/lib/mod/inspection/paper/_paper_food_inspection_report.cfm?inspectionID=140185&wgdmn=431&wguid=1367&wgunm=sysact"
 	]
+
+	def nodeSnag(self,selector, xpathNode, index):
+		arr = selector.xpath(xpathNode).extract()
+		if index+1 > len(arr):
+			return ''
+		else:	
+			return selector.xpath(xpathNode).extract()[index].strip()
 
 	def parse(self, response):
 		sel = Selector(response)
 		item = HealthiItem()
 		item["response_url"] = response.request.url
-		item["establishment_name"] = sel.xpath('//div[@class="container"]/text()').extract()[1].strip()
-		item["address"] =  sel.xpath('//div[@style="float:left; width:30%;"]/text()').extract()[0].strip()
-		item["city_state_zip"] =  sel.xpath('//div[@class="container"]/text()').extract()[5].strip()
-		item["email_address"] =  sel.xpath('//div[@class="container"]/text()').extract()[9].strip()
-		item["license_customer_number"] =  sel.xpath('//div[@class="container"]/text()').extract()[31].strip()
-		item["name_of_licensed_trash_or_solid_waste_contractor"] = sel.xpath('//div[@class="container"]/text()').extract()[52].strip()
-		item["name_of_licensed_liquid_grease_collections_transport_contractor"] = sel.xpath('//div[@class="container"]/text()').extract()[53].strip()
-		item["name_of_licensed_pest_exterminator_contractor"] = sel.xpath('//div[@class="container"]/text()').extract()[54].strip()
-		item["establishment_type"] = sel.xpath('//div[@class="container"]/text()').extract()[48].strip()
-		item["cfpm_number"] = sel.xpath('//div[@class="container"]/text()').extract()[51].strip()
-		item["certified_food_protection_manager"] = sel.xpath('//div[@class="container"]/text()').extract()[49].strip()
-		item["cfpm_expiration_date"] = sel.xpath('//span[@style="text-decoration:underline;"]/text()').extract()[0].strip()
-		item["telephone"] =  sel.xpath('//div[@class="container"]').xpath('div[@style="float:left; width:28%;"]/text()')[0].extract().strip()
-		item["date_of_inspection"] =  sel.xpath('//div[@class="container"]').xpath('div[@style="float:left; width:5%;"]/text()')[0].extract().strip() + '/' + sel.xpath('//div[@class="container"]').xpath('div[@style="float:left; width:5%;"]/text()')[1].extract().strip() + '/' + sel.xpath('//div[@class="container"]').xpath('div[@style="float:left; width:8%;"]/text()')[0].extract().strip()
-		item["time_in"] = sel.xpath('//div[@class="container"]').xpath('div[@style="float:left; width:5%;"]/text()')[2].extract().strip() + ':' + sel.xpath('//div[@class="container"]').xpath('div[@style="float:left; width:5%;"]/text()')[3].extract().strip() + sel.xpath('//div[@class="container"]').xpath('span/sup/text()')[0].extract().strip()
-		item["time_out"] = sel.xpath('//div[@class="container"]').xpath('div[@style="float:left; width:5%;"]/text()')[4].extract().strip() + ':' + sel.xpath('//div[@class="container"]').xpath('div[@style="float:left; width:5%;"]/text()')[5].extract().strip() + sel.xpath('//div[@class="container"]').xpath('span/sup/text()')[1].extract().strip()
-		item["license_holder"] =  sel.xpath('//div[@class="container"]').xpath('div[@style="float:left; width:30%;"]/text()')[1].extract().strip()
-		item["risk_category"] = sel.xpath('//div[@style="height:5px;width:5px;background-color:#FF0000;"]').xpath('preceding::span[1]/text()').extract()[0].strip()		
-		item["type_of_inspection"] = sel.xpath('//table[2]//tr[7]//div[last()]/text()')[0].extract().strip()
-		item["license_period_start"] = sel.xpath('//table[2]//tr[7]//div[2]/text()')[0].extract().strip() + '/' + sel.xpath('//table[2]//tr[7]//div[3]/text()')[0].extract().strip() + '/' + sel.xpath('//table[2]//tr[7]//div[4]/text()')[0].extract().strip()
-		item["license_period_end"] = sel.xpath('//table[2]//tr[7]//div[5]/text()')[0].extract().strip() + '/' + sel.xpath('//table[2]//tr[7]//div[6]/text()')[0].extract().strip() + '/' + sel.xpath('//table[2]//tr[7]//div[7]/text()')[0].extract().strip()
-		
+		item["establishment_name"] = self.nodeSnag(sel,'//div[@class="container"]/text()',1)
+		item["address"] =  self.nodeSnag(sel,'//div[@style="float:left; width:30%;"]/text()',0)
+		item["city_state_zip"] =  self.nodeSnag(sel,'//div[@class="container"]/text()',5)
+		item["email_address"] =  self.nodeSnag(sel,'//div[@class="container"]/text()',9)
+		item["license_customer_number"] =  self.nodeSnag(sel,'//div[@class="container"]/text()',31)
+		item["name_of_licensed_trash_or_solid_waste_contractor"] = self.nodeSnag(sel,'//div[@class="container"]/text()',52)
+		item["name_of_licensed_liquid_grease_collections_transport_contractor"] = self.nodeSnag(sel,'//div[@class="container"]/text()',53)
+		item["name_of_licensed_pest_exterminator_contractor"] = self.nodeSnag(sel,'//div[@class="container"]/text()',54)
+		item["establishment_type"] = self.nodeSnag(sel,'//div[@class="container"]/text()',48)
+		item["cfpm_number"] = self.nodeSnag(sel,'//div[@class="container"]/text()',51)
+		item["certified_food_protection_manager"] = self.nodeSnag(sel,'//div[@class="container"]/text()',49)
+		item["cfpm_expiration_date"] = self.nodeSnag(sel,'//span[@style="text-decoration:underline;"]/text()',0)
+		item["telephone"] =  self.nodeSnag(sel,'//div[@class="container"]//div[@style="float:left; width:28%;"]/text()',0)
+		item["date_of_inspection"] =  self.nodeSnag(sel,'//div[@class="container"]//div[@style="float:left; width:5%;"]/text()',0) + '/' + self.nodeSnag(sel,'//div[@class="container"]//div[@style="float:left; width:5%;"]/text()',1) + '/' + self.nodeSnag(sel,'//div[@class="container"]/div[@style="float:left; width:8%;"]/text()',0)
+		item["time_in"] = self.nodeSnag(sel,'//div[@class="container"]//div[@style="float:left; width:5%;"]/text()',2) + ':' + self.nodeSnag(sel,'//div[@class="container"]//div[@style="float:left; width:5%;"]/text()',3) + self.nodeSnag(sel,'//div[@class="container"]/span/sup/text()',0)
+		item["time_out"] = self.nodeSnag(sel,'//div[@class="container"]//div[@style="float:left; width:5%;"]/text()',4) + ':' + self.nodeSnag(sel,'//div[@class="container"]//div[@style="float:left; width:5%;"]/text()',5) + self.nodeSnag(sel,'//div[@class="container"]/span/sup/text()',1)
+		item["license_holder"] =  self.nodeSnag(sel,'//div[@class="container"]//div[@style="float:left; width:30%;"]/text()',1)
+		item["risk_category"] = self.nodeSnag(sel,'//div[@style="height:5px;width:5px;background-color:#FF0000;"]//preceding::span[1]/text()',0)		
+		item["type_of_inspection"] = self.nodeSnag(sel,'//table[2]//tr[7]//div[last()]/text()',0)
+		item["license_period_start"] = self.nodeSnag(sel,'//table[2]//tr[7]//div[2]/text()',0) + '/' + self.nodeSnag(sel,'//table[2]//tr[7]//div[3]/text()',0) + '/' + self.nodeSnag(sel,'//table[2]//tr[7]//div[4]/text()',0)
+		item["license_period_end"] = self.nodeSnag(sel,'//table[2]//tr[7]//div[5]/text()',0) + '/' + self.nodeSnag(sel,'//table[2]//tr[7]//div[6]/text()',0) + '/' + self.nodeSnag(sel,'//table[2]//tr[7]//div[7]/text()',0)
 		item["critical_violations"] = {}
-		item["critical_violations"]["total"] = sel.xpath('//table[@class="times"][2]/tr[2]/td[2]/text()')[0].extract().strip()
-		item["critical_violations"]["cos"] = sel.xpath('//table[@class="times"][2]/tr[2]/td[4]/text()')[0].extract().strip()
-		item["critical_violations"]["r"] = sel.xpath('//table[@class="times"][2]/tr[2]/td[6]/text()')[0].extract().strip()
-
+		item["critical_violations"]["total"] = self.nodeSnag(sel,'//table[@class="times"][2]/tr[2]/td[2]/text()',0)
+		item["critical_violations"]["cos"] = self.nodeSnag(sel,'//table[@class="times"][2]/tr[2]/td[4]/text()',0)
+		item["critical_violations"]["r"] = self.nodeSnag(sel,'//table[@class="times"][2]/tr[2]/td[6]/text()',0)
 		item["noncritical_violations"] = {}
-		item["noncritical_violations"]["total"] = sel.xpath('//table[@class="times"][2]/tr[3]/td[2]/text()')[0].extract().strip()
-		item["noncritical_violations"]["cos"] = sel.xpath('//table[@class="times"][2]/tr[3]/td[4]/text()')[0].extract().strip()
-		item["noncritical_violations"]["r"] = sel.xpath('//table[@class="times"][2]/tr[3]/td[6]/text()')[0].extract().strip()
+		item["noncritical_violations"]["total"] = self.nodeSnag(sel,'//table[@class="times"][2]/tr[3]/td[2]/text()',0)
+		item["noncritical_violations"]["cos"] = self.nodeSnag(sel,'//table[@class="times"][2]/tr[3]/td[4]/text()',0)
+		item["noncritical_violations"]["r"] = self.nodeSnag(sel,'//table[@class="times"][2]/tr[3]/td[6]/text()',0)
 		
 		return item
