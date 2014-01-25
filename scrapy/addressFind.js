33aad8b85a36a576aa4878a90b083f40ea24d393
Zach Cowell <zach.cowell@gmail.com>
2014-01-25 16:36:34 -0500

    script changes and mobile line added for layout

--- scrapy/addressFind.js
+++ scrapy/addressFind.js
@@ -1,2 +1,10 @@
 var add_list = db.inspections.aggregate([{ $group: {"_id": {address: "$address",city: "$city_state_zip"}}}]).result;
-for (x in add_list) { var ca = add_list[x]._id.address + ', ' + add_list[x]._id.city; print(ca); }
+var i = 0;
+for (x in add_list) { 
+	if (i < 3)
+	{
+		var ca = add_list[x]._id.address + ', ' + add_list[x]._id.city; 
+		print(ca); 
+	}
+	i++;
+}
--- scrapy/addressPrintout.sh
+++ scrapy/addressPrintout.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+i=0
+(mongo healthi addressFind.js) | while read line
+do
+	if [$i > 1] then
+   		echo $line
+   	fi
+done
 
-mongo healthi addressFind.js
 # add in bash script to parse the list and perform an http request to geocode each address
