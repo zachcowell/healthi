9ee0cdd13abb9d1515082b39e6e6bd5774c3a7a4
Zach Cowell <zach.cowell@gmail.com>
2014-01-27 23:49:20 -0500

    progress on geocoding method

--- routes/administration.js
+++ routes/administration.js
@@ -7,7 +7,10 @@ var Geocodio = require('geocodio');
 exports.geocoding = function(req,res){
 	var geocodio = new Geocodio({ api_key: '2ee87d6bec6e27f6575dc00ded1c28c56ec6856' });
 
-	var q = Inspections.aggregate([{ $group: {"_id": {address: "$address",city: "$city_state_zip"}}}]);
+	var q = Inspections.aggregate([
+		{ $match : { lat: null, lng: null } }, 
+		{ $group: {"_id": {address: "$address",city: "$city_state_zip"}} }
+	]);
 	var post_data = [];
 	var reports_affected = 0;	
 	var i = 0;
@@ -15,7 +18,7 @@ exports.geocoding = function(req,res){
 	q.exec(function (err,data){
 		for (x in data) { 
 			i++;
-			if (i < 4){
+			if (i < 2){
 				post_data.push({
 					address: data[x]._id.address,
 					city: data[x]._id.city,
@@ -28,14 +31,20 @@ exports.geocoding = function(req,res){
 	})
 	.then(function(){
 		_.each(post_data,function(item){
-			item.lat = 300;
-			item.lng = 500;
+			geocodio.geocode(item.full, function(err, response){
+    			if (err) console.log(err);
+    			else {
+    				if (response.results.length > 0){
+    					var resp = response.results[0].response.results[0];
+		    			item.lat = parseFloat(resp.location.lat);
+		    			item.lng = parseFloat(resp.location.lng);
+	    			}
+    				
+	    		}
+			});
 		});
 	})
-	.then(function(){
-		console.log(post_data);
-	})
-	.then(function(){
+	/*.then(function(){
 		_.each(post_data,function(item){
 			Inspections.update(
 	            {address: item.address, city_state_zip: item.city},
@@ -43,15 +52,11 @@ exports.geocoding = function(req,res){
 	            {upsert: false, multi: true},
 	            function(err,numberAffected,rawResponse){
 	                    if (err) console.log(err);
-	                    else { 
-	                    	reports_affected += parseInt(numberAffected); 
-	                    	console.log(reports_affected);
-	                    }
 	            });
 		});
-	})
+	})*/
 	.then(function(){
-		res.send('Reports affected: ' + reports_affected);
+		res.send(post_data);
 	});
 
 
