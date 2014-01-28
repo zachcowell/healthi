b864529bb270cbba12e621c90bd69595a68d329e
Zach Cowell <zach.cowell@gmail.com>
2014-01-27 21:17:30 -0500

    trying out a promise structure rather than diving into callback hell

--- routes/administration.js
+++ routes/administration.js
@@ -2,36 +2,57 @@ var Inspections = require('../models/inspections.js');
 var _ = require('underscore');
 var moment = require('moment');
 var request = require('request');
+var Geocodio = require('geocodio');
 
+exports.geocoding = function(req,res){
+	var geocodio = new Geocodio({ api_key: '2ee87d6bec6e27f6575dc00ded1c28c56ec6856' });
 
-exports.addressLatLng = function(req, res) {
 	var q = Inspections.aggregate([{ $group: {"_id": {address: "$address",city: "$city_state_zip"}}}]);
 	var post_data = [];
 	var reports_affected = 0;	
+	var i = 0;
+
 	q.exec(function (err,data){
 		for (x in data) { 
-			var addr = data[x]._id.address;
-			var csz = data[x]._id.city;
-
-			request.get('http://api.geocod.io/v1/geocode?api_key=2ee87d6bec6e27f6575dc00ded1c28c56ec6856&q='+addr + ', ' + csz,		    
-				function (error, response, body) {
-				    if (!error && response.statusCode == 200) {
-				    	var resp = JSON.parse(body);
-				        if (resp.results.length >0){
-				        	var lati = parseFloat(resp.results[0].location.lat);
-				        	var longi = parseFloat(resp.results[0].location.lng);
-				    		Inspections.update(
-								{address: addr, city_state_zip: csz},
-								{lat: lati, lng: longi},
-								{upsert: false, multi: true},
-								function(err,numberAffected,rawResponse){
-									if (err) console.log(err);
-									else { reports_affected += numberAffected; }
-								});
-				    	}
-				    }
+			i++;
+			if (i < 4){
+				post_data.push({
+					address: data[x]._id.address,
+					city: data[x]._id.city,
+					full: data[x]._id.address + ', ' + data[x]._id.city,
+					lat: null,
+					lng: null
 				});
+			}
 		}			
+	})
+	.then(function(){
+		_.each(post_data,function(item){
+			item.lat = 300;
+			item.lng = 500;
+		});
+	})
+	.then(function(){
+		console.log(post_data);
+	})
+	.then(function(){
+		_.each(post_data,function(item){
+			Inspections.update(
+	            {address: item.address, city_state_zip: item.city},
+	            {lat: item.lat, lng: item.lng},
+	            {upsert: false, multi: true},
+	            function(err,numberAffected,rawResponse){
+	                    if (err) console.log(err);
+	                    else { 
+	                    	reports_affected += parseInt(numberAffected); 
+	                    	console.log(reports_affected);
+	                    }
+	            });
+		});
+	})
+	.then(function(){
+		res.send('Reports affected: ' + reports_affected);
 	});
+
 
 }
