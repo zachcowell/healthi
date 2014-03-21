9181c200efa6ac59e75b03200a6e22c9fcf6b6b0
Zach Cowell <zach.cowell@gmail.com>
2014-03-21 01:19:55 -0400

    autonomy complete

--- scrapy/dataImport.sh
+++ scrapy/dataImport.sh
@@ -2,7 +2,7 @@
 LOGFILE="log"
 echo "=================== $(date +"%m.%d.%y") Data Pull ===================" >> $LOGFILE
 echo "[$(date)] Begin scraping Dept of Health website" >> $LOGFILE
-scrapy crawl single -o data.json
+scrapy crawl healthi -o data.json
 echo "[$(date)] Scrape complete" >> $LOGFILE
 echo "[$(date)] Purging existing database" >> $LOGFILE
 mongo healthi preimport.js
@@ -17,6 +17,14 @@ mongoimport --jsonArray --file ../geocoding/1391046983502.json -d healthi -c geo
 mongoimport --jsonArray --file ../geocoding/1391094811139.json -d healthi -c geotemp
 mongoimport --jsonArray --file ../geocoding/1391095425663.json -d healthi -c geotemp
 mongo healthi geocoding.js
-echo "[$(date)] Post import cleanup" >> $LOGFILE
+echo "[$(date)] Post-import" >> $LOGFILE
+mongo healthi postimport.js >> $LOGFILE
+echo "[$(date)] Dumping healthi database to file" >> $LOGFILE
+mongodump --db healthi
+echo "[$(date)] Pushing healthi to production" >> $LOGFILE
+mongo --eval "db.inspections.drop()" -u rootu -p bravo ds027749.mongolab.com:27749/healthi
+mongorestore -h ds027749.mongolab.com:27749 -d healthi -u rootu -p bravo dump/healthi
+echo "[$(date)] Cleanup" >> $LOGFILE
+rm -rf dump
 rm data.json
-mongo healthi postimport.js
+echo "[$(date)] Done!" >> $LOGFILE
--- scrapy/postimport.js
+++ scrapy/postimport.js
@@ -1,1 +1,6 @@
+var geotempCount = db.geotemp.find().count();
+var inspectionCount = db.inspections.find().count();
+print('Number of items in geotemp: ' + geotempCount);
+print('Number of items in inspection: ' + inspectionCount);
 db.geotemp.remove();
+db.geotemp.drop();
