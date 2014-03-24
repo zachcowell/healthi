1e7eb8a766d766c3353e0424b7c0315525f00a39
Zach Cowell <zach.cowell@gmail.com>
2014-03-24 00:55:14 -0400

    adjustments to import script

--- scrapy/dataImport.sh
+++ scrapy/dataImport.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 LOGFILE="log"
+DATAFILE="$(date +"%m%d%y").json"
+
 echo "=================== $(date +"%m.%d.%y") Data Pull ===================" >> $LOGFILE
 echo "[$(date)] Begin scraping Dept of Health website" >> $LOGFILE
-scrapy crawl healthi -o data.json
+scrapy crawl healthi -o $DATAFILE
 echo "[$(date)] Scrape complete" >> $LOGFILE
 echo "[$(date)] Purging existing database" >> $LOGFILE
 mongo healthi preimport.js
 echo "[$(date)] Importing data into datastore" >> $LOGFILE
-mongoimport --file data.json -d healthi -c inspections
+mongoimport --file $DATAFILE -d healthi -c inspections
 echo "[$(date)] Performing post import task" >> $LOGFILE
 mongo healthi import.js
 echo "[$(date)] Geocoding addresses" >> $LOGFILE
@@ -26,5 +28,5 @@ mongo --eval "db.inspections.drop()" -u rootu -p bravo ds027749.mongolab.com:277
 mongorestore -h ds027749.mongolab.com:27749 -d healthi -u rootu -p bravo dump/healthi
 echo "[$(date)] Cleanup" >> $LOGFILE
 rm -rf dump
-rm data.json
+mv $DATAFILE pastimports/
 echo "[$(date)] Done!" >> $LOGFILE
