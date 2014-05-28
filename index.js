948e74187c92db331a2b203db88bf7c26aa19c32
Zach Cowell <zach.cowell@gmail.com>
2014-05-27 20:39:31 -0400

    master branch gitignore

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ var express = require('express'),
 	admin = require('./routes/administration.js'),
 	http = require('http'),
 	path = require('path'),
+	credentials = require('./routes/credentials.js'),
 	app = module.exports = express();
 
+var env = 'production';
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-
     next();
 }
 
 app.configure(function(){
-	app.set('env','production');
+	app.set('env',env);
 	app.set('port', process.env.PORT || 3000);
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
@@ -29,13 +30,8 @@ app.configure(function(){
 	app.use(app.router);
 });
 
-if (app.get('env') === 'development') { 
-	app.use(express.errorHandler()); 
-	mongoose.connect('mongodb://localhost/healthi'); 
-}
-if (app.get('env') === 'production') { 
-	mongoose.connect('mongodb://rootu:bravo@ds027749.mongolab.com:27749/healthi'); 
-};
+if (env === 'development') { app.use(express.errorHandler()); }
+mongoose.connect(credentials.getKey(env));
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -50,18 +46,13 @@ app.get('/latest', api.latest);
 app.get('/worst/restaurantsavg', api.worstRestaurantsAvg);
 app.get('/worst/recentinspection', api.worstRecentInspections);
 app.get('/yelpBiz/:yelp_id', api.yelpBiz);
-
 app.get('/observations/:reportId', api.observations);
-
 app.get('/worst/inspections', api.worstInspections);
 app.get('/worst/repeatcriticals', api.worstRepeats);
 app.post('/name', api.name);
 app.post('/restaurantNames', api.restaurantNames);
 app.get('/partials/:name', routes.partials);
 app.get('/metrics/incidentBreakdown',metrics.violation_breakdown)
-
-//app.get('/yelpcode',admin.yelpBusinessCoding);
-
 app.get('*', routes.index);
 
 http.createServer(app).listen(app.get('port'), function () {
