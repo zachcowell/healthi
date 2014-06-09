0e3cde040a89f3018d29cababe0982355b5fe0c9
Zach Cowell <zach.cowell@gmail.com>
2014-06-08 23:41:47 -0400

    Prepping credentials for production

--- routes/credentials.js
+++ routes/credentials.js
@@ -1,8 +1,1 @@
-
-var credentialKeys = {
-	'development' : 'mongodb://localhost/healthi',
-	'production' : 'some-credentials-here'
-
-};
-
-exports.getKey = function(key){ return credentialKeys[key]; }
+var credentialKeys = {'development' : 'mongodb://localhost/healthi', 'production' : 'mongodb://rootu:bravo@ds027749.mongolab.com:27749/healthi'}; exports.getKey = function(key){ return credentialKeys[key]; }
